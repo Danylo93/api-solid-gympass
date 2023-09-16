08935d13224d103f9181df8e436bd5e113c32b26
Danylo Oliveira <danyloalves@prefeitura.sp.gov.br>
2023-09-16 14:22:02 -0300

    handler of global errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,24 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.register(appRoutes)
 
-// prisma.
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation errors', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: DATADOG/NEWRELIC/SENTRY
+  }
+
+  return reply.status(500).send({ message: 'Internal Server error.' })
+})
